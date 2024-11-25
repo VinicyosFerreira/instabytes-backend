7373912c30f4bc0200fd392f789e46d2089d42b2
Vinicyos Ferreira <vinifr987@gmail.com>
2024-11-25 10:49:42 -0300

    deploy do projeto

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './src/routes/postsRoutes.js';
+
+
+const app = express();
+routes(app)
+app.use(express.static("uploads"));
+
+app.listen(3000 , () => {
+    console.log("Servidor em QAP");
+});
